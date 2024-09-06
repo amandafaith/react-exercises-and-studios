02d00f839c134502a7c8446bc667816cf72b4c85
Amanda <amandafaithdev@gmail.com>
2024-09-05 19:49:09 -0500

    React 3 Studio

--- part3/studio/src/components/BoardAssignment.jsx
+++ part3/studio/src/components/BoardAssignment.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-export default function BoardAssignment () {
-   const boards = [];
+export default function BoardAssignment() {
+  const [boardName, setName] = useState("no boards yet!");
+  const boards = [
+    { label: "Strawberry Desserts", value: "Strawberry Desserts" },
+    { label: "Desserts", value: "Desserts" },
+    { label: "Tea Cakes", value: "Tea Cakes" },
+  ];
 
-   const handleChange = (event) => {
-   }
+  const handleChange = (event) => {};
 
-   return (
-      <div style={{paddingTop: "50px"}}>
+  return (
+    <div style={{ paddingTop: "50px" }}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+        {boards.map((board) => (
+          <option value={board.value}>{board.label}</option>
+        ))}
       </select>
 
       <p>Saved to {boardName}!</p>
-      </div>
-   );
+    </div>
+  );
 }
